2a73e8c322a2cd031e678ee6e347d85e3d6044ce
fonzi <fonzi@pop-os.localdomain>
2023-02-14 23:53:43 -0600

    Added conversion from emoji to text

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+const { countryCodeEmoji, emojiCountryCode } = require('country-code-emoji');
 
 
 test('Get Remotive.com QA Jobs', async ({ page }) => {
@@ -6,29 +7,32 @@ test('Get Remotive.com QA Jobs', async ({ page }) => {
   const jobs = await page.locator(".job-tile").all();
   let current = 0;
   let max = 20;
-  const regex = /(\p{Emoji}|\p{Extended_Pictographic})/gu;
-
-  //let emojiBs: RegExp = /\p{RI}\p{RI}|\p{Emoji}(\p{EMod}|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})?(\u{200D}\p{Emoji}(\p{EMod}|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})?)*|./gus;
   const lstJobs: headers[] = [];
   for (const job in jobs) {
     const text = await jobs[job].innerText();
     const title = text.split("\n")[0];
-    const location =  await jobs[job].locator(".left-tag").innerText();
+    const location = await jobs[job].locator(".left-tag").innerText();
 
-    const locationText = await location.split("\n")[0].toString();
-    //const sanitizedLocationName = locationText.replace(regex, '');
+    let locationText: any;
+    try {
+      locationText = emojiCountryCode(location.split("\n")[0]);
+    }
+    catch (e) {
+      locationText = location.split("\n")[0].toString();
+    }
 
     await jobs[job].hover();
     const moreInfo = await jobs[job].locator("a").nth(3).getAttribute("href");
-    const time = await jobs[job].locator(".tw-w-auto").innerText(); 
+    const time = await jobs[job].locator(".tw-w-auto").innerText();
 
-    lstJobs.push({title: title, location: locationText, date: time, moreInfo: moreInfo});
+    lstJobs.push({ title: title, location: locationText, date: time, moreInfo: moreInfo });
 
-    current ++;
-    if(current == max) break;
+    current++;
+    if (current == max) break;
   }
-
+  console.log("================QA REMOTIVE JOBS================");
   console.table(lstJobs);
+  console.log("================================================");
 });
 
 interface headers {
