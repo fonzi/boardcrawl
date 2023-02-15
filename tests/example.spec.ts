37ce5c60c62a31db7c078004e9cf93c7bf61e8c6
fonzi <fonzi@pop-os.localdomain>
2023-02-15 00:21:10 -0600

    Extracing logic

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,22 @@ const { countryCodeEmoji, emojiCountryCode } = require('country-code-emoji');
 
 
 test('Get Remotive.com QA Jobs', async ({ page }) => {
-  await page.goto('https://remotive.com/remote-jobs/qa');
+  const lstJobs = await getRemotiveJobs(page, 'https://remotive.com/remote-jobs/qa');
+  console.log("=================REMOTIVE JOBS=================");
+  console.table(lstJobs);
+  console.log("================================================");
+});
+
+
+test('Get Remotive.com Devops Jobs', async ({ page }) => {
+  const lstJobs = await getRemotiveJobs(page, 'https://remotive.com/remote-jobs/devops');
+  console.log("=================REMOTIVE JOBS=================");
+  console.table(lstJobs);
+  console.log("================================================");
+});
+
+async function getRemotiveJobs(page: any, url: string) {
+  await page.goto(url);
   const jobs = await page.locator(".job-tile").all();
   let current = 0;
   let max = 20;
@@ -12,28 +27,21 @@ test('Get Remotive.com QA Jobs', async ({ page }) => {
     const text = await jobs[job].innerText();
     const title = text.split("\n")[0];
     const location = await jobs[job].locator(".left-tag").innerText();
-
     let locationText: any;
     try {
       locationText = emojiCountryCode(location.split("\n")[0]);
-    }
-    catch (e) {
+    } catch (e) {
       locationText = location.split("\n")[0].toString();
     }
-
     await jobs[job].hover();
     const moreInfo = await jobs[job].locator("a").nth(3).getAttribute("href");
     const time = await jobs[job].locator(".tw-w-auto").innerText();
-
     lstJobs.push({ title: title, location: locationText, date: time, moreInfo: moreInfo });
-
     current++;
     if (current == max) break;
   }
-  console.log("================QA REMOTIVE JOBS================");
-  console.table(lstJobs);
-  console.log("================================================");
-});
+  return lstJobs;
+}
 
 interface headers {
   title: string;
