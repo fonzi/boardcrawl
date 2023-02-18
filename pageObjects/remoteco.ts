02d73270c4095511ee1a9b14862f5c9622ca02a5
fonzi <me@fonzi.xyz>
2023-02-17 22:59:59 -0600

    Added remote.co, need to do a bit of cleanup

--- pageObjects/remoteco.ts
+++ pageObjects/remoteco.ts
@@ -2,7 +2,7 @@ import { expect, test, Locator, type Page } from '@playwright/test'
 
 export default class Remoteco {
 
-    readonly noGutterSelector = ".row.no-gutters";
+    readonly noGutterSelector = ".card.m-0.border-left-0.border-right-0.border-top-0.border-bottom";
 
     constructor(private page: Page) {
         this.page = page;
@@ -12,10 +12,27 @@ export default class Remoteco {
     public async getRemotecoJobs(url: string) {
         await this.page.goto(url);
         const noGutterRows = await this.page.locator(this.noGutterSelector).all();
-        
+        const lstJobs: headers[] = [];
         for(const row in noGutterRows) {
-            const text = await noGutterRows[row].innerText();
-            console.log(text);
+            const badge = await noGutterRows[row].locator(".badge.badge-success").nth(0).innerText();
+            const title = await noGutterRows[row].locator(".font-weight-bold.larger").innerText();
+            const date = await noGutterRows[row].locator("date").innerText();
+            const company = await (await noGutterRows[row].locator(".m-0.text-secondary").innerText()).split('|')[0].split(" ")[0];
+            const moreInfo = await noGutterRows[row].getAttribute("href");
+            lstJobs.push({ title: title, company: company, date: date, moreInfo: "https://remote.co"+ moreInfo });
         }
+        return lstJobs;
     }
 }
+
+interface headers {
+    title: string;
+    company: string;
+    date: string;
+    moreInfo: any;
+}
+
+//   title: string;
+//   location: string;
+//   date: string;
+//   moreInfo: any;
--- tests/remoteco.spec.ts
+++ tests/remoteco.spec.ts
@@ -7,8 +7,8 @@ test.beforeEach(async ({ page }) => {
   remoteco = new Remoteco(page);
 });
 
-test('Get Remotive.com QA Jobs', async ({ page }) => {
+test('Get Remote.co QA Jobs', async ({ page }) => {
   const lstJobs = await remoteco.getRemotecoJobs('https://remote.co/remote-jobs/qa/');
-  console.log("=================REMOTIVE JOBS=================");
+  console.log("=================REMOTECO JOBS=================");
   console.table(lstJobs);
 });
